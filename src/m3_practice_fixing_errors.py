0e976d21bc334541559fb9ac756fb79f048dc40e
wilcoxeb <wilcoxeb@rose-hulman.edu>
2018-11-28 14:34:15 -0500

    m3 session 2

--- src/m3_practice_fixing_errors.py
+++ src/m3_practice_fixing_errors.py
@@ -6,7 +6,7 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 """  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ###############################################################################
-# TODO: 2.
+# Done: 2.
 #   Locate the syntax (notation) errors in this file
 #   by looking for red underlines.
 #
@@ -32,6 +32,7 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 
 import rosegraphics as rg
 import math
+
 
 def main():
     """ Calls the other functions in this module to demo them. """
@@ -60,9 +61,9 @@ def turtle_fun():
     alan.pen = rg.Pen('blue', 30)
     alan.paint_bucket = rg.PaintBucket('yellow')
 
-    alan.backwards(3 * (47 + 16))
+    alan.backward(3 * (47 + 16))
     alan.begin_fill()
-    alan.(25)
+    alan.draw_circle(25)
     alan.end_fill()
 
     alan.forward(200)
@@ -72,4 +73,6 @@ def turtle_fun():
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # -----------------------------------------------------------------------------
+
+
 main()
